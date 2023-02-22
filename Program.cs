ad86fa018a1a95703db4d7f6d5b9813dbbc62ed1
√ｲﾶﾑム <113216701+magnusekvojtech@users.noreply.github.com>
2023-02-22 12:11:44 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Kalkulačka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahoj, Tohle je Jednoduchá Kalkulačka");
+            Console.WriteLine("Napiš první číslo☺");
+            //uděláme proměnné
+            string cislo1 = Console.ReadLine();
+            double a = Convert.ToDouble(cislo1);
+            Console.WriteLine("Napiš druhé číslo☺");
+            string cislo2 = Console.ReadLine();
+            double b = Convert.ToDouble(cislo2);
+            //zadáme co proměnné budou dělat
+            double c;
+            double d;
+            double e;
+            double f;
+            c = a + b;
+            d = a - b;
+            e = a * b;
+            f = a / b;
+            //vypíšeme proměnné
+            Console.WriteLine("Zde jsou výsledky");
+            Console.WriteLine("Scítání" + c);
+            Console.WriteLine("Odčítaní" + d);
+            Console.WriteLine("Násobení" + e);
+            Console.WriteLine("Dělení" + f);
+            Console.WriteLine("Děkuji za použití programu♥");
+            //ukončíme program
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
